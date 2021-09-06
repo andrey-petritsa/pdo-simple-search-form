19ecfd6de61480dff62bbcf8d22f505bc59a45ec
andrey-petritsa <andrey@petritsa.ru>
2021-09-06 16:59:41 +0300

    Добавил запросы на создание БД

---
+++ sql/create-db.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE inline_exam;
+use inline_exam;
+
+CREATE TABLE Posts
+(
+    postId int,
+    userId int,
+    title  varchar(100),
+    body   varchar(255),
+    PRIMARY KEY (postId)
+);
+
+CREATE TABLE Comments
+(
+    commentId int,
+    postId int,
+    name varchar(100),
+    email varchar(255),
+    body varchar(255),
+    FOREIGN KEY (postId)
+        REFERENCES Posts(postId)
+        ON DELETE CASCADE
+);
