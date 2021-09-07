dc3793a4da71ec25878f801f0b09b15586b6dcd8
andrey-petritsa <andrey@petritsa.ru>
2021-09-07 12:18:53 +0300

    Добавил вставку постов в БД

--- src/script/download-post.php
+++ src/script/download-post.php
@@ -6,6 +6,8 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . '../../');
 $dotenv->load();
 
+mysqli_report(MYSQLI_REPORT_ALL);
+
 function get_json_from_url($url)
 {
     $ch = curl_init($url);
@@ -17,11 +19,18 @@ function get_json_from_url($url)
     return json_decode($page);
 }
 
-$conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$mysql_cursor = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
+if ($mysql_cursor->connect_error) {
+    die("Connection failed: " . $mysql_cursor->connect_error);
 }
 
-$conn->close();
+$prepared_query_insert_post = $mysql_cursor->prepare("INSERT INTO Posts(postId, userId, title, body) VALUES (?, ?, ?, ?)");
+$posts = get_json_from_url($_ENV['POSTS_URL']);
+foreach ($posts as $post) {
+    $prepared_query_insert_post->bind_param('iiss', $post->id, $post->userId, $post->title, $post->body);
+    $prepared_query_insert_post->execute();
+}
+
+$mysql_cursor->close();
 
 
