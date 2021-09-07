5c14135301e89267f944991baa42a34b55a7094e
andrey-petritsa <andrey@petritsa.ru>
2021-09-07 13:09:25 +0300

    Добавил вывод информации о загруженных записях и комментариях

--- src/script/download-post.php
+++ src/script/download-post.php
@@ -24,19 +24,25 @@ if ($mysql_cursor->connect_error) {
     die("Connection failed: " . $mysql_cursor->connect_error);
 }
 
+$total_posts_inserted = 0;
 $prepared_query_insert_post = $mysql_cursor->prepare("INSERT IGNORE INTO Posts(postId, userId, title, body) VALUES (?, ?, ?, ?)");
 $posts = get_json_from_url($_ENV['POSTS_URL']);
 foreach ($posts as $post) {
     $prepared_query_insert_post->bind_param('iiss', $post->id, $post->userId, $post->title, $post->body);
     $prepared_query_insert_post->execute();
+    $total_posts_inserted += $mysql_cursor->affected_rows;
 }
 
+$total_comments_inserted = 0;
 $prepared_query_insert_comments = $mysql_cursor->prepare("INSERT IGNORE INTO Comments(commentId, postId, name, email, body) VALUES (?, ?, ?, ?, ?)");
 $comments = get_json_from_url($_ENV['COMMENTS_URL']);
 foreach ($comments as $comment) {
     $prepared_query_insert_comments->bind_param('iisss', $comment->id, $comment->postId, $comment->name, $comment->email, $comment->body);
     $prepared_query_insert_comments->execute();
+    $total_comments_inserted += $mysql_cursor->affected_rows;
 }
+
+echo "Загружено $total_posts_inserted записей и Y $total_comments_inserted комментариев";
 
 $mysql_cursor->close();
 
