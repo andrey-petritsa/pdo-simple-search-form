2206530f9350e7611ef2d89c796ef0ce1535e905
andrey-petritsa <andrey@petritsa.ru>
2021-09-07 15:54:04 +0300

    Добавил валидацию на стороне сервера

--- src/public/php/find-post.php
+++ src/public/php/find-post.php
@@ -1,3 +1,11 @@
 <?php
+header('Content-type: application/json');
+$MIN_COMMENT_CONTAINED_TEXT_LENGHT = 3;
 $_POST = json_decode(file_get_contents("php://input"), true);
 $comment_contained_text = $_POST['post-comment-contain-text'];
+
+if (strlen($comment_contained_text) < $MIN_COMMENT_CONTAINED_TEXT_LENGHT) {
+    echo json_encode(['message' => "Поддерживается поиск не меньше $MIN_COMMENT_CONTAINED_TEXT_LENGHT символов"]);
+    return;
+}
+
