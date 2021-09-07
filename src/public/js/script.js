1e64453249890d5e35fe9a136a4d7c2efcc09f56
andrey-petritsa <andrey@petritsa.ru>
2021-09-07 15:26:04 +0300

    Добавил очистку формы при условии, что валидация прошла успешна на стороне клиента

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,11 +1,12 @@
 let MIN_INPUT_TEXT_LENGHT = 3
 
 let form = document.querySelector('.search-form')
+let form_input = document.querySelector('.search-form__input')
+let form_alert = document.querySelector('.search-form__alert');
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-    let form_input = document.querySelector('.search-form__input')
+    form_alert.innerHTML = ''
     if (form_input.value.length < MIN_INPUT_TEXT_LENGHT) {
-        let form_alert = document.querySelector('.search-form__alert');
         form_alert.innerHTML = `Для поиска нужно минимум ${MIN_INPUT_TEXT_LENGHT} символа`
     }
 })
